ba240e3d76c12d4cfe7ee36b55567077af09cb45
DevKarolBezerra <karolinebezerrac@gmail.com>
2024-11-11 18:51:33 -0300

    first commit

--- employees/urls.py
+++ employees/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import EmployeeViewSet, AgeReportView, SalaryReportView
 
 router = DefaultRouter()
@@ -9,4 +10,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('reports/employees/age/', AgeReportView.as_view(), name='employee-age-report'),
     path('reports/employees/salary/', SalaryReportView.as_view(), name='employee-salary-report'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Endpoint para obter o token
 ]
--- ssys_employee_manager/settings.py
+++ ssys_employee_manager/settings.py
@@ -17,8 +17,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'employees',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
