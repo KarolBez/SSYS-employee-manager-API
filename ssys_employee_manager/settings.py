782872e1bb1eac831acfc4560012b588edb44e0e
DevKarolBezerra <karolinebezerrac@gmail.com>
2024-11-10 23:55:31 -0300

    first commit

--- ssys_employee_manager/settings.py
+++ ssys_employee_manager/settings.py
@@ -1,18 +1,15 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-g*!^*%9y8(l*l^u&0x8axl(yqzfle(w6l1$(w8&aqu%ut@bb)+'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
